4853b312b2662c140cc4bbdbcf74f8fa3be698d5
c.nasuhoglu@gmail.com <c.nasuhoglu@gmail.com>
2023-04-02 14:39:01 +0000

    europe shipment

---
+++ models/src/src_shipments.sql
@@ -0,0 +1,15 @@
+WITH shipments AS (select * from SHIPMENT_DATAMART.SHIPMENT_DATAMART.shipments )
+SELECT shipment_id,
+  kn_com_ref,
+  is_dang_good,
+  packages,
+  gross_weight,
+  volume,
+  from_location,
+  to_location,
+  shipment_type_code,
+  shipment_type_name,
+  transport_type,
+  last_update,
+  is_active
+  from shipments
