59cf5ce46a284e40091917a88734ff55a1d28930
c.nasuhoglu@gmail.com <c.nasuhoglu@gmail.com>
2023-04-04 06:30:50 +0000

    shipments models for air, overland and sea

---
+++ models/transport_type/sea_shipments.sql
@@ -0,0 +1,12 @@
+{% if is_incremental() %}
+  -- Incremental query
+  SELECT *
+  FROM {{ ref('src_shipments') }}
+  WHERE transport_type = 'S'
+    AND last_update > (SELECT MAX(last_update) FROM {{ this }})
+{% else %}
+  -- Full-refresh query
+  SELECT *
+  FROM {{ ref('src_shipments') }}
+  WHERE transport_type = 'S'
+{% endif %}
